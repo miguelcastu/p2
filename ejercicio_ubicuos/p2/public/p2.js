14784b66b8218fd452f62d6b13c78debfc253c6c
migue <miguelcastu27@gmail.com>
2023-04-18 09:44:47 +0200

    socket movil

---
+++ ejercicio_ubicuos/p2/public/p2.js
@@ -0,0 +1,12 @@
+const socket=io()
+document.getElementById("videos-button").onclick = function () {
+    location.href = "content1/videos.html";
+  };
+  
+  document.getElementById("series-button").onclick = function () {
+    location.href = "content1/series.html";
+  };
+  
+  document.getElementById("peliculas-button").onclick = function () {
+    location.href = "content1/peliculas.html";
+  };
--- ejercicio_ubicuos/p2/servidor.js
+++ ejercicio_ubicuos/p2/servidor.js
@@ -1,33 +1,22 @@
+const path =require('path');
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
 
-// Configurar la API RESTful
-app.get('/api/data', (req, res) => {
-  const data = { message: 'Hola desde la API RESTful!' };
-  res.json(data);
+app.set('port',process.env.PORT || 3000);
+
+app.use(express.static(path.join(__dirname,'public')));
+
+const server=app.listen(app.get('port'), () => {
+  console.log(`Servidor en escucha en el puerto`, app.get('port'));
 });
 
-// Configurar Socket.io
+
+//whitesockets
+const socketIO = require('socket.io');
+const io = socketIO(server);
 io.on('connection', (socket) => {
-  console.log(`Nuevo cliente conectado: ${socket.id}`);
-  
-  // Manejar eventos del cliente
-  socket.on('evento-del-cliente', (data) => {
-    console.log(`El cliente envió el siguiente mensaje: ${data.message}`);
-    
-    // Enviar mensaje de vuelta al cliente
-    io.to(socket.id).emit('evento-del-servidor', { message: 'Mensaje del servidor' });
-  });
+  console.log("new connection", socket.id);
 });
 
-// Iniciar el servidor
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Servidor en escucha en el puerto ${PORT}`);
-});
+
 
