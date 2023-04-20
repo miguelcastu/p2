51e658407c29661d5e39effc4f6ed120454f0dae
migue <miguelcastu27@gmail.com>
2023-04-20 15:10:41 +0200

    asadsadsa

--- www/inicio_qr.js
+++ www/inicio_qr.js
@@ -3,7 +3,7 @@ window.onload = function() {
 const socket = io();
 
 const qrcode = new QRCode("qrcode", {
-  text: "https://socket-io-pointer.andreabellucci1.repl.co/pointer/pointer.html",
+  text: "http://localhost:8080/pointer/pointer.html",
   width: 512,
   height: 512,
   colorDark: "#000000",
